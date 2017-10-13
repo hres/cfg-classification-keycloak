1839b293e63e4cf48fc7f10a6c1171512f0b0ee8
luc-bertrand <luc.bertrand@canada.ca>
2017-10-13 09:53:18 -0400

    Update export-keycloak-key.sh

--- scripts/ssl/keycloak/export-keycloak-key.sh
+++ scripts/ssl/keycloak/export-keycloak-key.sh
@@ -1,2 +1,2 @@
-keytool -importkeystore -srckeystore application.keystore -destkeystore keystore.p12 -deststoretype PKCS12
+sudo keytool -importkeystore -srckeystore application.keystore -destkeystore keystore.p12 -deststoretype PKCS12
 openssl pkcs12 -in keystore.p12 -nodes -nocerts -out keycloak.pem
