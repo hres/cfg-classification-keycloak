98d00125ef733b93a33b502f4f017db7fbcfa8af
luc-bertrand <luc.bertrand@canada.ca>
2017-10-13 09:14:21 -0400

    Update createKeystore.sh

--- scripts/ssl/keycloak/createKeystore.sh
+++ scripts/ssl/keycloak/createKeystore.sh
@@ -1,1 +1,1 @@
-keytool -genkey -alias keycloak -keyalg RSA -keystore application.keystore -validity 10950
+sudo keytool -genkey -alias keycloak -keyalg RSA -keystore application.keystore -validity 10950
