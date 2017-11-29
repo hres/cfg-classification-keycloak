7d69380ccc52709553fff2066bc2082a6fc50854
luc-bertrand <luc.bertrand@canada.ca>
2017-11-29 12:52:03 -0500

    Update create-tomcat-cert.sh

--- scripts/ssl/rootCA/create-tomcat-cert.sh
+++ scripts/ssl/rootCA/create-tomcat-cert.sh
@@ -1,1 +1,1 @@
-openssl x509 -req -in tomcat.csr -CA luc-bertrand-CA.pem -CAkey luc-bertrand-CA.key -CAcreateserial -out tomcat.crt -days 500 -sha256 -extfile tomcat.ext
+openssl x509 -req -in tomcat.csr -CA ../luc-bertrand-CA.pem -CAkey ../luc-bertrand-CA.key -CAcreateserial -out tomcat.crt -days 500 -sha256 -extfile tomcat.ext
