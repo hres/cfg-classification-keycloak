0a2c32d81ab84be3c1e2f9704fe90f596fdc2b9a
luc-bertrand <luc.bertrand@canada.ca>
2018-02-27 12:17:53 -0500

    Update import-keycloak-certkey.sh

--- scripts/ssl/tomcat/import-keycloak-certkey.sh
+++ scripts/ssl/tomcat/import-keycloak-certkey.sh
@@ -1,2 +1,2 @@
 sudo openssl pkcs12 -export -in keycloak.crt -inkey keycloak.pem -name keycloak -out keycloak.p12
-sudo keytool -importkeystore -deststorepass password -destkeystore ../tomcat.keystore -srckeystore keycloak.p12 -srcstoretype PKCS12
+sudo keytool -importkeystore -deststorepass <enter password here> -destkeystore tomcat.keystore -srckeystore keycloak.p12 -srcstoretype PKCS12
