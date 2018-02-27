336a0b0cf0aef1128f8c3e6c639190e80316f672
luc-bertrand <luc.bertrand@canada.ca>
2018-02-27 10:58:43 -0500

    Update create-apache-csr.sh

--- scripts/ssl/apache2/create-apache-csr.sh
+++ scripts/ssl/apache2/create-apache-csr.sh
@@ -1,1 +1,1 @@
-sudo openssl req -new -newkey rsa:2048 -nodes -keyout apache.key -out apache.csr -subj "/C=CA/ST=Ontario/L=Ottawa/O/CN=lam-dev.canadaeast.cloudapp.azure.com"
+sudo openssl req -new -newkey rsa:2048 -nodes -keyout apache.key -out apache.csr -subj "/C=CA/ST=Ontario/L=Ottawa/O=Health Canada/OU=IMSD/CN=lam-dev.canadaeast.cloudapp.azure.com"
