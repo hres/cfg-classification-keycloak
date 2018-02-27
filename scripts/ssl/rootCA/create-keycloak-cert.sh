6c44d1c53f33248fe60a9a7c0ba401e776443029
luc-bertrand <luc.bertrand@canada.ca>
2018-02-27 11:56:14 -0500

    Update create-keycloak-cert.sh

--- scripts/ssl/rootCA/create-keycloak-cert.sh
+++ scripts/ssl/rootCA/create-keycloak-cert.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-openssl x509 -req -in keycloak.csr -CA luc-bertrand-CA.pem -CAkey luc-bertrand-CA.key -CAcreateserial -out keycloak.crt -days 500 -sha256 -extfile keycloak.ext
+openssl x509 -req -in keycloak.csr -CA cfg-food-modernization-CA.pem -CAkey cfg-food-modernization-CA.key -CAcreateserial -out keycloak.crt -days 500 -sha256 -extfile keycloak.ext
