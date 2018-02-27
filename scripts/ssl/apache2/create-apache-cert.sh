173683b9b1dfa8869ecc21ec5dd628c820eda680
luc-bertrand <luc.bertrand@canada.ca>
2018-02-27 10:44:02 -0500

    Update create-apache-cert.sh

--- scripts/ssl/apache2/create-apache-cert.sh
+++ scripts/ssl/apache2/create-apache-cert.sh
@@ -1,1 +1,1 @@
-openssl x509 -req -in apache.csr -CA luc-bertrand-CA.pem -CAkey luc-bertrand-CA.key -CAcreateserial -out apache.crt -days 500 -sha256 -extfile apache.ext
+openssl x509 -req -in apache.csr -CA cfg-food-modernization-CA.pem -CAkey cfg-food-modernization-CA.key -CAcreateserial -out apache.crt -days 500 -sha256 -extfile apache.ext
