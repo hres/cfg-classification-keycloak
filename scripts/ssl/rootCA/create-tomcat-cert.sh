dfcf386594554ea05b2ab605382251cd86ec13eb
luc-bertrand <luc.bertrand@canada.ca>
2018-02-28 10:00:44 -0500

    Update create-tomcat-cert.sh

--- scripts/ssl/rootCA/create-tomcat-cert.sh
+++ scripts/ssl/rootCA/create-tomcat-cert.sh
@@ -1,1 +1,1 @@
-openssl x509 -req -in tomcat.csr -CA cfg-food-modernization-CA.pem -CAkey cfg-food-modernization-CA.key -CAcreateserial -out tomcat.crt -days 500 -sha256 -extfile tomcat.ext
+sudo openssl x509 -req -in tomcat.csr -CA cfg-food-modernization-CA.pem -CAkey cfg-food-modernization-CA.key -CAcreateserial -out tomcat.crt -days 500 -sha256 -extfile tomcat.ext
